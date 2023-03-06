51063382c57c1db40541d227dc88270d30b77b3a
GitHadi0722 <hadiblogs222003@gmail.com>
2023-03-06 09:55:36 -0800

    ds

---
+++ calc.js
@@ -0,0 +1,15 @@
+const display =document.querySelector("#display");
+		const button =document.querySelector("button");
+        buttons.forEach((btn) =>{
+        	btn.addEventListener("click", () => {
+        		if (btn.id ==="=") {
+                   display.value = eval(display.value);
+        		} else if (btn.id ==="AC") {
+        			display.value = "";
+        		} else if (btn.id == "DE") {
+        			display.value = display.value.slice(0, -1);
+        		} else {
+        			display.value += btn.id;
+        		}
+        	});
+        });
